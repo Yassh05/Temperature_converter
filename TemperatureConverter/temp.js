54a8593285e33aa9218c3ebda0e085f2a1436118
Yash Sharma <167101572+Yassh05@users.noreply.github.com>
2025-02-04 21:13:23 +0530

    Add files via upload

---
+++ TemperatureConverter/temp.js
@@ -0,0 +1,27 @@
+const value1= document.getElementById("temp");
+const ftc=document.getElementById("ftc");
+const ctf=document.getElementById("ctf");
+const answer=document.getElementById("answer");
+let temp1;
+
+
+function convert(){
+    if(ftc.checked){
+         temp1=Number(value1.value);
+         temp1=  (temp1-32)*(5/9);
+         answer.textContent= temp1.toFixed(2)+ " celcius";
+    }
+    else if(ctf.checked){
+        temp1=Number(value1.value);
+        temp1=temp1 * 9/5 + 32;
+        answer.textContent= temp1.toFixed(2)+  " fahrenheit";
+    }
+    else{
+        answer.textContent="select a unit";
+    }
+
+};
+
+let x=[1,2,3,4,5];
+let y=Math.max(...x);
+console.log(y);
